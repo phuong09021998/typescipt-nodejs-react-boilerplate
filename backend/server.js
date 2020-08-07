47a32c8c3425a88106195b6a401bbca234e31658
Robert Nguyen <phuong09021998@gmail.com>
2020-08-07 16:35:53 +0700

    Finish docker

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,27 @@
-import express from 'express';
+const express = require('express');
+const path = require('path');
 // const mongoose = require('mongoose');
 // const morgan = require('morgan');
 // const bodyParser = require('body-parser');
 // const cors = require('cors');
 
-// require('dotenv').config();
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+const isProduction = process.env.NODE_ENV === 'production';
 
 // App
 const app = express();
 
-app.listen(3000);
+if (isProduction) {
+  // Serve dist files if is production mode
+  const distPath = path.resolve(__dirname, '../frontend/dist/prod');
+  app.use(express.static(distPath));
+  app.get('*', (_, res) => {
+    res.sendFile(`${distPath}/index.html`);
+  });
+}
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
--- webpack.config.prod.babel.js
+++ webpack.config.prod.babel.js
@@ -1,8 +1,6 @@
 import path from 'path';
 import merge from 'webpack-merge';
 
-import OfflinePlugin from 'offline-plugin';
-
 import BaseWebpackConfig from './webpack.config.base.babel';
 
 export default merge(BaseWebpackConfig, {
@@ -23,12 +21,6 @@ export default merge(BaseWebpackConfig, {
     // https://webpack.js.org/configuration/output/#output-publicpath
     publicPath: '/',
   },
-
-  // A list of used webpack plugins
-  plugins: [
-    // It's always better if OfflinePlugin is the last plugin added
-    new OfflinePlugin(),
-  ],
 
   // Source map mode
   // https://webpack.js.org/configuration/devtool
